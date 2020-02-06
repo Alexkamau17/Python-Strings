9f019d3fceaf3d0abcf8d12722ff017ff910752f
Alexkamau17 <60480324+Alexkamau17@users.noreply.github.com>
2020-02-06 19:47:31 +0300

    Add files via upload

---
+++ stringLec.py
@@ -0,0 +1,89 @@
+'''
+a="hello world"
+#spliting and slicing of strings
+#outputs the first letter in the string
+#print(a[0])
+
+#outputs the letters in the range given 2:5
+#print(a[2:5])
+
+#outputs the letter at the end of the string
+#print(a[-1])
+
+
+#print(a[:6])
+
+print(a[:])
+
+print(a[0:])
+
+#prints the 2nd letter upto the 3rd letter
+print("letters in a[2:4]: ",a[2:4])
+
+#prints the whitespace
+#print(a[5])
+
+#includes the 6th letter and excludes the 10th letter
+#print(a[6:10])
+
+a="hello"
+b="world"
+#outputs the values a and b(concanting)
+#print(a+b)
+
+#outputs a five times
+#print(a*5)
+#checks if h is in the string
+#print('h' in a)
+
+#checks if m is in the string
+#print('m' in a)
+
+#checks whether there's x in the string
+#print('x' not in a)
+
+
+#string built-in methods (len(),lower(),upper(),capitalize())
+x="hello Alex"
+y="HELLO"
+
+
+#outputs the length of the string
+print(len(x))
+
+#outputs the strings as  lower case
+print(y.lower())
+
+#outputs the string as upper case
+print(x.upper())
+
+#outputs the first letter as uppercase and the rest lowercase
+print(x.capitalize())
+
+
+#function for capturing an input form user
+print("Enter:")
+b=input()
+print("Name," + b)
+'''
+'''
+#replaces an item in the list
+list="toyota,suzuki"
+m=list.replace("toyota", "subaru")
+print(m)
+'''
+'''
+#outputs the binary of the integer given
+z=bin(45)
+print(z)
+'''
+'''
+#outputs the complex number
+y=complex(7,9)
+print(y)
+'''
+'''
+#formats the 16 to  hex format
+y=format(16, 'x')
+print(y)
+'''
